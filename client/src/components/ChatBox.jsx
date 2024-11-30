acc705e69e0972f2ed9cf4f0a61cf25008786136
Anirudh Singh <129600428+anirudh-singh01@users.noreply.github.com>
2024-11-30 18:25:28 +0530

    Update ChatBox.jsx

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Box, Input, Button, HStack, VStack, Text, Avatar } from "@chakra-ui/react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+//const socket = io("http://localhost:4000"); //local
+const socket = io("https://real-time-chat-application-8tst.onrender.com"); //deployment
 
 const ChatBox = () => {
   const [messages, setMessages] = useState([]);
